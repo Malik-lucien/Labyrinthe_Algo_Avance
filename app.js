32b25290664abcb56746df5ff38321592f920c59
malik.aouane <malik.aouane@le-campus-numerique.fr>
2020-11-17 15:13:55 +0100

    app & index change

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
+let arrayLab;
+
+
 function randomOrNot() {
     let random = true;
     let content = document.getElementById('grid-container');
-    while(content.firstChild) {
+    while (content.firstChild) {
         content.removeChild(content.firstChild);
     }
     if (random) {
-        randomLab();
-    }else {
-        chooseLab()
+        let randomCase = Math.floor(Math.random() * Object.keys(labyrinthes).length + 2);
+        let randomEx = Math.floor(Math.random() * 3);
+        new_labyrinthe(randomCase, labyrinthes[randomCase]["ex-" + randomEx]);
+    } else {
+        new_labyrinthe(6, labyrinthes["6"]["ex-0"]);
     }
 }
-/*===============CODE GENERANT LE LABYRINTHE=========================*/
-function randomLab() {
-    let randomCase = Math.floor(Math.random() * Object.keys(labyrinthes).length);
-    let randomEx = Math.floor(Math.random() * 3);
-    new_labyrinthe(randomCase, labyrinthes[randomCase]["ex-" + randomEx])
-}
 
-function chooseLab() {
-    new_labyrinthe(6, labyrinthes["6"]["ex-0"]);
-}
+/*===============CODE GENERANT LE LABYRINTHE=========================*/
 
 function new_labyrinthe(taille, ex) {
+    arrayLab = new Array(ex.length);
 
     document.getElementById("grid-container").style.gridTemplateColumns = "repeat(" + taille + ", 50px)";
     document.getElementById("grid-container").style.gridTemplateRows = "repeat(" + taille + ", 50px)";
 
-
-    //haut droit bas gauche 
+    //haut droit bas gauche
     for (let i = 0; i < ex.length; i++) {
         let borderstyle = "";
-
+        arrayLab[i] = new Array(4);
         for (let j = 0; j < ex[i]["walls"].length; j++) {
 
             if (ex[i]["walls"][j]) {
@@ -39,36 +36,39 @@ function new_labyrinthe(taille, ex) {
                 borderstyle = borderstyle + "none ";
             }
         }
+        arrayLab[i]["x"] = ex[i]["posX"];
+        arrayLab[i]["y"] = ex[i]["posY"];
+        arrayLab[i]["walls"] = ex[i]["walls"];
+        arrayLab[i]["isVisited"] = false;
 
-        console.log(borderstyle, "celule num " + ex[i]["posX"] +"_"+ ex[i]["posY"]);
         let element = document.createElement("DIV");
-        element.id = "cellule_"+ ex[i]["posX"] +"_"+ ex[i]["posY"];
-        if(i === ex.length-1){
-            element.style.backgroundColor = "tomato"}
+        element.id = "cellule_" + ex[i]["posX"] + "_" + ex[i]["posY"];
+        if (i === ex.length - 1) {
+            element.style.backgroundColor = "tomato"
+        }
         element.style.borderStyle = borderstyle;
         element.style.borderColor = "rgb(210,10,122)";
         document.getElementById("grid-container").appendChild(element);
-
     }
-    move(3);
-}
-
-/*==============CODE SE DEPLACANT DANS LE LABYRINTHE=================*/
-function move(nbCote) {
-    let posX = 3;
-    let posY = 0;
-    let element = document.createElement("DIV");
-    element.style.backgroundColor = "rgb(64,234,207)"
-    while (lastCase(nbCote, posX, posY)) {
-        findMyPosition(posX, posY)
-
-    }
-}
-function findMyPosition(X, Y) {
-    document.getElementById("cellule_" + X +"_" + Y).id
+    console.log(arrayLab)
 }
 
 
-function lastCase(nbCote, X, Y) {
-    return (X === nbCote) && (Y === nbCote);
-}
+// /*==============CODE SE DEPLACANT DANS LE LABYRINTHE=================*/
+// function move(nbCote) {
+//     let posX = 3;
+//     let posY = 0;
+//     let element = document.createElement("DIV");
+//     element.style.backgroundColor = "rgb(64,234,207)"
+//     while (lastCase(nbCote, posX, posY)) {
+//         findMyPosition(posX, posY)
+//     }
+// }
+//
+// function findMyPosition(X, Y) {
+//     document.getElementById("cellule_" + X +"_" + Y).id
+// }
+//
+// function lastCase(nbCote, X, Y) {
+//     return (X === nbCote) && (Y === nbCote);
+// }
